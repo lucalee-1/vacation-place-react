109d944c3129f631b9d142cd7f10f37ff516728c
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-06-07 09:01:06 +0900

    Add first routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './views/Explore';
+import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
+
 function App() {
-  return <div><h1>App</h1></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+      <h1>App</h1>
+    </>
+  );
 }
 
 export default App;
