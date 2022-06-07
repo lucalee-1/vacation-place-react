45424c5811cf704d47b401040ff6de3dd30af14a
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-06-07 09:57:53 +0900

    Add categories to explore page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Explore from './views/Explore';
+import Category from './views/Category';
 import SignIn from './views/SignIn';
 import SignUp from './views/SignUp';
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Explore />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<SignIn />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
--- src/views/Explore.jsx
+++ src/views/Explore.jsx
@@ -1,5 +1,36 @@
+import { Link } from 'react-router-dom';
+import beachCategoryImage from '../assets/beachCategory.jpg';
+
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div>
+      <header>
+        <p>Explore</p>
+      </header>
+
+      <main>
+        <p>Categories</p>
+        <div>
+          <Link to="/category/beach" >
+          <img className="" alt="" />
+          <p>Beach</p>
+          </Link>
+          <Link to="/category/mountain" >
+          <img className="" alt="" />
+          <p>Mountain</p>
+          </Link>
+          <Link to="/category/city" >
+          <img className="" alt="" />
+          <p>City</p>
+          </Link>
+          <Link to="/category/luxury" >
+          <img className="" alt="" />
+          <p>Luxury</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Explore;
